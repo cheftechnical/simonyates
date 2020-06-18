d94aa5bd66d165f2a07fd86714f5b549eb8b115e
Simon Yates <simon@whiip.com>
2020-06-18 02:16:41 -0400

    Update grid sizing

--- public/my-app/src/pages/About/Hero.tsx
+++ public/my-app/src/pages/About/Hero.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import {Grid} from "@material-ui/core";
+import {Grid} from '@material-ui/core';
 
 export default function Hero() {
     return (
         <Grid container>
-            <Grid item>
-                Simon Yates,
-                Programmer from Toronto
+            <Grid item xs={6}>
+                Simon Yates,<br/>
+                <strong>Programmer</strong> from Toronto
             </Grid>
-            <Grid item>
+            <Grid item xs={6}>
                 [photo]
             </Grid>
         </Grid>
