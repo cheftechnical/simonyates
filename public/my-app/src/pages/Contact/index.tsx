e6b7631a8e07b24e8649e344d7b646d5b18e70e8
Simon Yates <simon@whiip.com>
2020-06-19 19:30:14 -0400

    Add the form components to the contact page

--- public/my-app/src/pages/Contact/index.tsx
+++ public/my-app/src/pages/Contact/index.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import DefaultLayout from '../../layouts/Default';
+import {Button, Grid, TextField} from '@material-ui/core';
 
 export default function Contact() {
     return (
         <DefaultLayout top="contact">
-            [contact placeholder]
+            <Grid container>
+                <Grid item xs={6}>
+                    <h1>Pleasure to meet you.</h1>
+                    <p>I&rsquo;m always open to make new connections and chat about software development.</p>
+
+                    <TextField fullWidth label="Name"/>
+                    <TextField fullWidth label="Email"/>
+                    <TextField fullWidth label="Subject"/>
+                    <TextField fullWidth multiline label="Message"/>
+                    <Button variant="contained">Send</Button>
+                </Grid>
+            </Grid>
         </DefaultLayout>
     );
 };
