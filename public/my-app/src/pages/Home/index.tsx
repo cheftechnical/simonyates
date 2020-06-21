74b17342ad231568398a1349bff8bbafe0c54da2
Simon Yates <simon@whiip.com>
2020-06-21 12:50:46 -0400

    Update homepage copy

--- public/my-app/src/pages/Home/index.tsx
+++ public/my-app/src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import DefaultLayout from '../../layouts/Default';
+import {Typography} from '@material-ui/core';
 
 export default function Home() {
     return (
         <DefaultLayout>
-            Hola
+            <Typography variant="h1">
+                Hi, I&rsquo;m Simon Yates.<br/>
+                I couldn&rsquo;t fit everything into LinkedIn,<br/>
+                so I made a website.
+            </Typography>
         </DefaultLayout>
     );
 };
