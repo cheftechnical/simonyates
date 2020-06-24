23375f765bf95154c026c6193fe242e2cc3bc9b3
Simon Yates <simon@whiip.com>
2020-06-24 19:31:11 -0400

    Update structure of page

--- src/pages/Work/Experience.tsx
+++ src/pages/Work/Experience.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import {Box, Button} from '@material-ui/core';
+import {Box, Button, Grid} from '@material-ui/core';
 import {Link} from '@material-ui/core';
 import {Link as ReactRouterDom} from 'react-router-dom';
 import Typography from '../../styling/Typography';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import {makeStyles} from '@material-ui/core/styles';
+import {color} from '../../styling/Color';
 
 interface Props {
 	children: any[] | any;
@@ -13,27 +15,40 @@ interface Props {
 	when: string;
 }
 
+const useStyles = makeStyles((theme) => ({
+	h3Link: {
+		color: color.grey['600']
+	}
+}));
+
 export default function Experience(props: Props) {
+	const classes = useStyles();
 	const {children, employer, href, title, when} = props;
 
 	return (
-		<Box display="flex">
-			<Box>
+		<Grid container>
+			<Grid item xs={1}>
 				<Link component={ReactRouterDom} to={href}>[logo]</Link>
-			</Box>
-			<Box>
-				<div>
-					<Typography group="primary" variant="h3" weight="regular">
-						<Link component={ReactRouterDom} to={href}>{employer}, {title}, {when}</Link>
-					</Typography>
-				</div>
+			</Grid>
+			<Grid item xs={11}>
+				<Typography group="primary" variant="h3" weight="regular">
+					<Link
+						className={classes.h3Link}
+						component={ReactRouterDom}
+						to={href}
+					>
+						{employer}, {title}, {when}
+					</Link>
+				</Typography>
+
 				<div>{children}</div>
+
 				<div>
 					<Button endIcon={<ArrowRightIcon/>} href={href} variant="text">
 						Read Full Details
 					</Button>
 				</div>
-			</Box>
-		</Box>
+			</Grid>
+		</Grid>
 	);
 };
