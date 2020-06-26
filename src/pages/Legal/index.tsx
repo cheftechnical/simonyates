74c2212029a831fd091d64dc6f6994fb4ad36652
Simon Yates <simon@whiip.com>
2020-06-26 02:18:11 -0400

    Wrapped the content in a Container component

--- src/pages/Legal/index.tsx
+++ src/pages/Legal/index.tsx
@@ -3,7 +3,7 @@ import DefaultLayout from '../../layouts/Default';
 import PrivacyPolicy from './PrivacyPolicy';
 import TermsAndConditions from './TermsAndConditions';
 import {SectionItem} from '../../components/Section/SectionItem';
-import {Grid} from '@material-ui/core';
+import {Container, Grid} from '@material-ui/core';
 import Sections from '../../components/Sections';
 import NavRight from '../../components/NavRight';
 import Section from '../../components/Section';
@@ -17,21 +17,23 @@ export default function Legal() {
 
 	return (
 		<DefaultLayout title="Legal">
-			<Grid container>
-				<Grid item xs={10}>
-					<Sections onChange={handleChange}>
-						<Section id="privacy-policy" name="Privacy Policy">
-							<PrivacyPolicy/>
-						</Section>
-						<Section id="terms-of-service" name="Terms of Service">
-							<TermsAndConditions/>
-						</Section>
-					</Sections>
+			<Container>
+				<Grid container>
+					<Grid item xs={10}>
+						<Sections onChange={handleChange}>
+							<Section id="privacy-policy" name="Privacy Policy">
+								<PrivacyPolicy/>
+							</Section>
+							<Section id="terms-of-service" name="Terms of Service">
+								<TermsAndConditions/>
+							</Section>
+						</Sections>
+					</Grid>
+					<Grid item xs={2}>
+						<NavRight sections={sections}/>
+					</Grid>
 				</Grid>
-				<Grid item xs={2}>
-					<NavRight sections={sections}/>
-				</Grid>
-			</Grid>
+			</Container>
 		</DefaultLayout>
 	);
 };
