8495bd6f8fa65497836f48dfd833d5ed5564f5b8
Simon Yates <simon@whiip.com>
2020-06-26 02:22:19 -0400

    Updated function description

--- src/styling/rem.ts
+++ src/styling/rem.ts
@@ -1,3 +1,10 @@
+/**
+ * Convert a value in pixels and return in rems.
+ *
+ * This function is useful for converting designs from Figma.
+ *
+ * @param value
+ */
 export default function rem(value: number) {
 	return (value/16) + 'rem'
 };
