1faecdc4dfd62d6081d9d7560b4b5244d7ff9238
Simon Yates <simon@whiip.com>
2020-07-25 15:16:09 -0400

    Fix spacing

--- src/pages/Featured/Download.tsx
+++ src/pages/Featured/Download.tsx
@@ -5,7 +5,7 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
 	download: {
-		marginTop: theme.spacing(8 / 8)
+		marginTop: theme.spacing(24 / 8)
 	}
 }));
 
