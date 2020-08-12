3857e9529b6d3bff42e45ec118420edf29e00151
Simon Yates <simon@whiip.com>
2020-08-11 23:42:37 -0400

    Update the mobile version of the component to use a carousel

--- src/pages/About/PhotoGallery/Photos.tsx
+++ src/pages/About/PhotoGallery/Photos.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import {Grid} from '@material-ui/core';
+import {Grid, Hidden} from '@material-ui/core';
 import Image from '../../../components/Image';
 import {makeStyles} from '@material-ui/core/styles';
 import rem from '../../../styling/rem';
 import MyGridContainer from '../../../styling/MyGridContainer';
+import Carousel from 'react-material-ui-carousel';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
 	root: {
 		paddingTop: rem(24)
+	},
+
+	carouselActive: {
+
+	},
+	carouselIndicator: {
+
 	}
 }));
 
@@ -16,20 +24,46 @@ export default function Photos() {
 
 	return (
 		<div className={classes.root}>
-			<MyGridContainer>
-				<Grid item xs={3}>
-					<Image alt="Jean'is Joplin" src="/images/about/photo-gallery/jean.png"/>
-				</Grid>
-				<Grid item xs={3}>
-					<Image alt="Mexico" src="/images/about/photo-gallery/mexico-1.png"/>
-				</Grid>
-				<Grid item xs={3}>
-					<Image alt="Mexico" src="/images/about/photo-gallery/mexico-2.png"/>
-				</Grid>
-				<Grid item xs={3}>
-					<Image alt="Mexico" src="/images/about/photo-gallery/mexico-3.png"/>
-				</Grid>
-			</MyGridContainer>
+			{/* Desktop */}
+			<Hidden smDown>
+				<MyGridContainer>
+					<Grid item xs={3}>
+						<Image alt="Jean'is Joplin" src="/images/about/photo-gallery/jean.png"/>
+					</Grid>
+					<Grid item xs={3}>
+						<Image alt="Mexico" src="/images/about/photo-gallery/mexico-1.png"/>
+					</Grid>
+					<Grid item xs={3}>
+						<Image alt="Mexico" src="/images/about/photo-gallery/mexico-2.png"/>
+					</Grid>
+					<Grid item xs={3}>
+						<Image alt="Mexico" src="/images/about/photo-gallery/mexico-3.png"/>
+					</Grid>
+				</MyGridContainer>
+			</Hidden>
+
+			{/*	Mobile */}
+			<Hidden mdUp>
+				<Carousel
+					autoPlay={false}
+					animation="slide"
+					activeIndicatorProps={{className: classes.carouselActive, style: {}}}
+					indicatorProps={{className: classes.carouselActive, style: {}}}
+				>
+					<div>
+						<Image alt="Jean'is Joplin" src="/images/about/photo-gallery/jean.png"/>
+					</div>
+					<div>
+						<Image alt="Mexico" src="/images/about/photo-gallery/mexico-1.png"/>
+					</div>
+					<div>
+						<Image alt="Mexico" src="/images/about/photo-gallery/mexico-2.png"/>
+					</div>
+					<div>
+						<Image alt="Mexico" src="/images/about/photo-gallery/mexico-3.png"/>
+					</div>
+				</Carousel>
+			</Hidden>
 		</div>
 	);
 };
