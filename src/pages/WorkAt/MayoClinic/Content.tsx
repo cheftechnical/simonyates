075e04aff3502c71772ecb2b903e53e240bfd2c4
Simon Yates <simon@whiip.com>
2020-12-02 01:31:08 -0500

    - Fixed a typo. Replaced double-quote with single-quote.

--- src/pages/WorkAt/MayoClinic/Content.tsx
+++ src/pages/WorkAt/MayoClinic/Content.tsx
@@ -47,7 +47,7 @@ export default function Content(props: Props) {
 					decision support. The System expands the &ldquo;recall of past experience&rdquo;
 					approach that a provider (physician) uses to formulate a course of action for a
 					given patient. By utilizing Big Data techniques, we enable the provider to recall
-					all similar patients from an institution&rdquo;s electronic medical record (EMR)
+					all similar patients from an institution&rsquo;s electronic medical record (EMR)
 					repository, to explore &ldquo;what-if&rdquo; scenarios, and to collect these
 					evidence-based cohorts for future statistical validation and pattern&nbsp;mining.
 				</Typography>
