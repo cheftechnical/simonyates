1f3dd764b393e0e7b417f69a8a9a552829bb5695
Simon Yates <simon@whiip.com>
2021-05-30 17:53:01 -0400

    Remove dead code

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,6 @@ import {HelmetProvider} from 'react-helmet-async';
 ReactDOM.render(
 	<React.StrictMode>
 		<HelmetProvider>
-			{/*<Helmet>*/}
-			{/*	<title>Simon Yates</title>*/}
-			{/*</Helmet>*/}
-
 			<App/>
 		</HelmetProvider>
 	</React.StrictMode>,
