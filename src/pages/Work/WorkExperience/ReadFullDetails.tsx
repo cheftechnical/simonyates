7ce1062cdaef23dd2013efe80166a6830fa8029f
Simon Yates <simon@whiip.com>
2021-06-05 15:27:06 -0400

    Add an aria-label to the read full details link

--- src/pages/Work/WorkExperience/ReadFullDetails.tsx
+++ src/pages/Work/WorkExperience/ReadFullDetails.tsx
@@ -4,24 +4,32 @@ import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import {makeStyles} from '@material-ui/core/styles';
 
 interface Props {
-	href: string;
+  /**
+   * aria-label
+   */
+  ariaLabel: string;
+
+  /**
+   * The URL to link to when the button is clicked. If defined, an a element will be used as the root node.
+   */
+  href: string;
 }
 
 const useStyles = makeStyles((theme) => ({
-	readFullDetails: {
-		marginTop: theme.spacing(32/8)
-	}
+  readFullDetails: {
+    marginTop: theme.spacing(32 / 8)
+  }
 }));
 
 export default function ReadFullDetails(props: Props) {
-	const classes = useStyles();
-	const {href} = props;
+  const classes = useStyles();
+  const {ariaLabel, href} = props;
 
-	return (
-		<div className={classes.readFullDetails}>
-			<Button endIcon={<ArrowRightIcon/>} href={href} variant="text">
-				Read Full Details
-			</Button>
-		</div>
-	);
+  return (
+    <div className={classes.readFullDetails}>
+      <Button aria-label={ariaLabel} endIcon={<ArrowRightIcon/>} href={href} variant="text">
+        Read Full Details
+      </Button>
+    </div>
+  );
 }
