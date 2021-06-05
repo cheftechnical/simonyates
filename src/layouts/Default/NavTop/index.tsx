d392e6d5e271d2326a8a96e4e831919e54718539
Simon Yates <simon@whiip.com>
2021-06-05 17:21:40 -0400

    Cleanup

--- src/layouts/Default/NavTop/index.tsx
+++ src/layouts/Default/NavTop/index.tsx
@@ -4,23 +4,22 @@ import ForMobile from './ForMobile';
 import ForDesktop from './ForDesktop';
 
 interface Props {
-	selected?: string;
+  selected?: string;
 }
 
 export default function NavTop(props: Props) {
-	const {selected} = props;
+  const {selected} = props;
 
-	const theme = useTheme();
-	const matchesXs = useMediaQuery(theme.breakpoints.down('xs'));
-	// const matchesMd = useMediaQuery(theme.breakpoints.down('md'));
+  const theme = useTheme();
+  const matchesXs = useMediaQuery(theme.breakpoints.down('xs'));
 
-	if (matchesXs) {
-		return (
-			<ForMobile selected={selected}/>
-		);
-	}
+  if (matchesXs) {
+    return (
+      <ForMobile selected={selected}/>
+    );
+  }
 
-	return (
-		<ForDesktop selected={selected}/>
-	);
+  return (
+    <ForDesktop selected={selected}/>
+  );
 };
