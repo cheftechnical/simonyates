258e9e478e1128722debe6657da337f9330b7fd9
Simon Yates <simon@whiip.com>
2021-06-19 19:21:50 -0400

    Fix the Storybook template to always include the Router and DefaultTheme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,21 @@
+import {addDecorator} from '@storybook/react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import DefaultTheme from '../src/styling/DefaultTheme';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+addDecorator(storyFn => (
+  <Router>
+    <DefaultTheme>
+      {storyFn()}
+    </DefaultTheme>
+  </Router>
+));
