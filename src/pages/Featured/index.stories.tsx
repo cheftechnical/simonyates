4f08235957c1846a6b2886ca87229979d41cf525
Simon Yates <simon@whiip.com>
2021-06-19 19:32:51 -0400

    Remove story for page

--- src/pages/Featured/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import Featured from './index';
-
-export default {title: 'Pages/Featured'};
-
-export const _default = () => <Featured/>;
--- src/pages/NoMatch/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import NoMatch from "./index";
-
-export default {title: 'Pages/404 Page Not Found'};
-
-export const _default = () => <NoMatch/>;
