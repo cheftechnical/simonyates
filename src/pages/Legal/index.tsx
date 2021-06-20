8aa285053fc49db71e7943b2b4bf915436225557
Simon Yates <simon@whiip.com>
2021-06-20 14:13:52 -0400

    Generalize the Section handling with a hook

--- src/pages/Legal/index.tsx
+++ src/pages/Legal/index.tsx
@@ -1,42 +1,37 @@
-import * as React from 'react';
 import DefaultLayout from '../../layouts/Default';
 import PrivacyPolicy from './PrivacyPolicy';
 import TermsAndConditions from './TermsAndConditions';
-import {SectionItem} from '../../components/Section/SectionItem';
 import {Grid} from '@material-ui/core';
 import Sections from '../../components/Sections';
 import NavRight from '../../components/NavRight';
 import Section from '../../components/Section';
 import MyContainer from '../../styling/MyContainer';
 import MyGridContainer from '../../styling/MyGridContainer';
+import {useSections} from '../../components/Sections/useSections';
 
 export default function Legal() {
-	const [sections, setSections] = React.useState<SectionItem[]>();
+  const [sections, handleSectionChange] = useSections();
 
-	const handleChange = React.useCallback((newValue: SectionItem[]) => {
-		setSections(newValue);
-	}, []);
-
-	return (
-		<DefaultLayout title="Legal">
-			<MyContainer>
-				<MyGridContainer>
-					<Grid item md={6} xs={12}>
-						<Sections onChange={handleChange}>
-							<Section id="privacy-policy" name="Privacy Policy">
-								<PrivacyPolicy/>
-							</Section>
-							<Section id="terms-of-service" name="Terms of Service">
-								<TermsAndConditions/>
-							</Section>
-						</Sections>
-					</Grid>
-					<Grid item md={3}/>
-					<Grid item md={3}>
-						<NavRight sections={sections}/>
-					</Grid>
-				</MyGridContainer>
-			</MyContainer>
-		</DefaultLayout>
-	);
+  return (
+    <DefaultLayout title="Legal">
+      <MyContainer>
+        <MyGridContainer>
+          <Grid item md={6} xs={12}>
+            <Sections onChange={handleSectionChange}>
+              <Section id="privacy-policy" name="Privacy Policy">
+                <PrivacyPolicy/>
+              </Section>
+              <Section id="terms-of-service" name="Terms of Service">
+                <TermsAndConditions/>
+              </Section>
+            </Sections>
+          </Grid>
+          <Grid item md={3}/>
+          <Grid item md={3}>
+            <NavRight sections={sections}/>
+          </Grid>
+        </MyGridContainer>
+      </MyContainer>
+    </DefaultLayout>
+  );
 };
