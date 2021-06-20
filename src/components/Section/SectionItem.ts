c1f7728ded6bab7b0dcd6e5f3f4fa2f8af528121
Simon Yates <simon@whiip.com>
2021-06-20 16:35:53 -0400

    Update comments

--- src/components/Section/SectionItem.ts
+++ src/components/Section/SectionItem.ts
@@ -1,4 +1,10 @@
 export type SectionItem = {
+  /**
+   * A unique ID that identifies the section, this will be used as the anchor id the link
+   */
   id: string;
+  /**
+   * The name of the section, and is the text which appears in the list of sections
+   */
   name: string;
 };
