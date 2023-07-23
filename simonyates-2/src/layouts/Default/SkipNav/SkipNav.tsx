23af287ff27aa99bb760d121ae59cba01f8cc7b4
Simon Yates <simon@sybaris.ca>
2023-07-23 18:39:11 -0400

    SkipNav works

--- simonyates-2/src/layouts/Default/SkipNav/SkipNav.tsx
+++ simonyates-2/src/layouts/Default/SkipNav/SkipNav.tsx
@@ -1,5 +1,6 @@
 // import {createStyles, makeStyles} from '@material-ui/core/styles';
 import {color} from '../../../styling/Color/Color';
+import {styled} from "@mui/material";
 
 /**
  * This component creates a hidden link that allows a screen reader to skip
@@ -64,6 +65,53 @@ import {color} from '../../../styling/Color/Color';
 //   }),
 // );
 
+const StyledA = styled('a')(({theme}) => ({
+    '&:focus': {
+      clip: 'auto',
+      height: 'auto',
+      left: 0,
+      margin: 0,
+      padding: '10px 0',
+      top: 0,
+      width: '100%',
+
+      /**
+       * The original source code included a styled background and border
+       * but I removed that from my implementation
+       */
+      // background: '#fdf6e7',
+      // border: '2px solid #990000',
+      // borderWidth: 2,
+      // borderStyle: 'solid',
+      // borderColor: color.red[500],
+      border: 'none',
+      borderLeft: 'none',
+      borderRight: 'none',
+      textAlign: 'center',
+      fontWeight: 'bold',
+
+      /**
+       * The original source code specified a custom red color for the
+       * focus event. I have changed this to use an on-brand shade of red.
+       */
+      // color: '#990000',
+      color: color.red[500],
+    }
+}))
+
+const StyledDivRoot = styled('div')(({theme}) => ({
+    border: 0,
+    clip: 'rect(0 0 0 0)', // disable this line if you want to debug
+    heightWidth: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 0,
+    whiteSpace: 'nowrap',
+    width: 1, // disable this line if you want to debug
+}));
+
 export interface Props {
   /**
    * The id of the main content which this component references
@@ -75,12 +123,7 @@ export default function SkipNav(props: Props) {
   // const classes = useStyles();
   const {mainId} = props;
 
-  // return (
-  //   <div className={classes.root}><a className={classes.a} href={`#${mainId}`}>Skip navigation</a></div>
-  // );
-
-  // @todo mui5
-  return(
-      <div>[SkipNav]</div>
-  )
+  return (
+    <StyledDivRoot><StyledA href={`#${mainId}`}>Skip navigation</StyledA></StyledDivRoot>
+  );
 }
