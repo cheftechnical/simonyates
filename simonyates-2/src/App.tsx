7821486c407d861b7d9ab0cedf7f88607236c7bc
Simon Yates <simon@sybaris.ca>
2023-07-27 15:11:21 -0400

    Cleanup NoMatch

--- simonyates-2/src/App.tsx
+++ simonyates-2/src/App.tsx
@@ -1,6 +1,7 @@
-import React, {lazy} from 'react';
+import React, {lazy, Suspense} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import ThemeProviderWrapper from "./styling/ThemeProviderWrapper";
+import {LinearProgress} from "@mui/material";
 
 const About = lazy(() => import('./pages/About/About'));
 const Featured = lazy(() => import('./pages/Featured'));
@@ -12,24 +13,26 @@ function App() {
   return (
     <ThemeProviderWrapper>
       <BrowserRouter>
-        <Routes>
-          {/*<Route path="/work/uncharted-software" component={UnchartedSoftware}/>*/}
-          {/*<Route path="/work/rbc" component={Rbc}/>*/}
-          {/*<Route path="/work/mayo-clinic" component={MayoClinic}/>*/}
-          {/*<Route path="/work/jib-design-and-advertising" component={JibDesignAndAdvertising}/>*/}
-          {/*<Route path="/work/infomart" component={Infomart}/>*/}
-          {/*<Route path="/work/genworth-canada" component={GenworthCanada}/>*/}
-          {/*<Route path="/resources/international" component={International}/>*/}
-          {/*<Route path="/figpie/trigonometry" component={FigpieTrigonometry}/>*/}
-          {/*<Route path="/figpie" component={Figpie}/>*/}
-          <Route path="/work" element={<Work/>}/>
-          {/*<Route path="/legal" component={Legal}/>*/}
-          <Route path="/featured" element={<Featured/>}/>
-          {/*<Route path="/contact" component={Contact}/>*/}
-          <Route path="/about" element={<About/>}/>
-          <Route path="/" element={<Home/>}/>
-          <Route path="*" element={<NoMatch/>}/>
-        </Routes>
+        <Suspense fallback={<LinearProgress/>}>
+          <Routes>
+            {/*<Route path="/work/uncharted-software" component={UnchartedSoftware}/>*/}
+            {/*<Route path="/work/rbc" component={Rbc}/>*/}
+            {/*<Route path="/work/mayo-clinic" component={MayoClinic}/>*/}
+            {/*<Route path="/work/jib-design-and-advertising" component={JibDesignAndAdvertising}/>*/}
+            {/*<Route path="/work/infomart" component={Infomart}/>*/}
+            {/*<Route path="/work/genworth-canada" component={GenworthCanada}/>*/}
+            {/*<Route path="/resources/international" component={International}/>*/}
+            {/*<Route path="/figpie/trigonometry" component={FigpieTrigonometry}/>*/}
+            {/*<Route path="/figpie" component={Figpie}/>*/}
+            <Route path="/work" element={<Work/>}/>
+            {/*<Route path="/legal" component={Legal}/>*/}
+            <Route path="/featured" element={<Featured/>}/>
+            {/*<Route path="/contact" component={Contact}/>*/}
+            <Route path="/about" element={<About/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<NoMatch/>}/>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </ThemeProviderWrapper>
   )
--- simonyates-2/src/pages/NoMatch/NoMatch.tsx
+++ simonyates-2/src/pages/NoMatch/NoMatch.tsx
@@ -1,10 +1,9 @@
 import DefaultLayout from '../../layouts/Default/Default';
-import Typography from '../../styling/Typography/Typography';
 import {color} from '../../styling/Color/Color';
 import {Link as ReactRouterDom} from 'react-router-dom';
 import rem from '../../styling/rem';
 import MyContainer from '../../styling/MyContainer/MyContainer';
-import {styled} from "@mui/material";
+import {styled, Typography} from "@mui/material";
 
 const StyledLink = styled(ReactRouterDom)(({theme}) => ({
   fontWeight: 'bold',
@@ -15,23 +14,23 @@ const StyledLink = styled(ReactRouterDom)(({theme}) => ({
 const StyledTypographyBody = styled(Typography)(({theme}) => ({
   marginBottom: theme.spacing(388 / 8),
   color: color.grey['600']
-}));
+})) as typeof Typography;
 
 const StyledTypographyHeadline = styled(Typography)(({theme}) => ({
   fontSize: rem(88),
   lineHeight: rem(88),
   marginBottom: theme.spacing(72 / 8),
   marginTop: theme.spacing(272 / 8),
-}));
+})) as typeof Typography;
 
 export default function NoMatch() {
   return (
     <DefaultLayout title="404: Page Not Found">
       <MyContainer>
-        <StyledTypographyHeadline group="primary" variant="h1" weight="regular">
+        <StyledTypographyHeadline component="h1" variant="primaryH1">
           404
         </StyledTypographyHeadline>
-        <StyledTypographyBody group="primary" variant="h3" weight="regular">
+        <StyledTypographyBody component="p" variant="primaryH3">
           Sorry, that page doesn&rsquo;t&nbsp;exist.<br/>
           But you can navigate back to&nbsp;<StyledLink to="/">home</StyledLink>.
         </StyledTypographyBody>
