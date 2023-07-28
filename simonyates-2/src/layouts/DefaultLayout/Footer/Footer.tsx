0a1917a6dd325c25fc39f534beb3afe82ea30ab3
Simon Yates <simon@sybaris.ca>
2023-07-28 19:55:12 -0400

    Removed dependency on custom Typography component

--- simonyates-2/src/layouts/DefaultLayout/Footer/Footer.tsx
+++ simonyates-2/src/layouts/DefaultLayout/Footer/Footer.tsx
@@ -1,12 +1,9 @@
-// import {Grid, Link} from '@material-ui/core';
-// import {makeStyles} from '@material-ui/core/styles';
-import {Link as ReactRouterDom} from 'react-router-dom';
+import {Link as ReactRouterDomLink} from 'react-router-dom';
 import Logo from '../../../components/Logo';
 import {color} from '../../../styling/Color/Color';
-import Typography from '../../../styling/Typography/Typography';
 import MyContainer from '../../../styling/MyContainer/MyContainer';
 import rem from '../../../styling/rem';
-import {Grid, Link, styled} from "@mui/material";
+import {Grid, Link, styled, Typography} from "@mui/material";
 
 // const useStyles = makeStyles((themeMui) => ({
 // 	root: {
@@ -32,76 +29,76 @@ import {Grid, Link, styled} from "@mui/material";
 // }));
 
 const StyledGridItemLeft = styled(Grid)(({theme}) => ({
-	paddingTop: theme.spacing(16/8),
+  paddingTop: theme.spacing(16 / 8),
 }))
 
 const StyledDivRoot = styled('div')(({theme}) => ({
-	paddingTop: theme.spacing(32 / 8),
-	paddingBottom: theme.spacing(24 / 8),
-	backgroundColor: color.grey['900']
+  paddingTop: theme.spacing(32 / 8),
+  paddingBottom: theme.spacing(24 / 8),
+  backgroundColor: color.grey['900']
 }));
 
 const StyledGridItemLogo = styled(Grid)(({theme}) => ({
-	padding: 0,
+  padding: 0,
 }));
 
 const StyledGridItemRight = styled(Grid)(({theme}) => ({
-	textAlign: 'right'
+  textAlign: 'right'
 }));
 
-const StyledReactRouterDomLink = styled(ReactRouterDom)(({theme}) => ({
-	color: color.grey['400']
-}));
+const StyledLink = styled(Link)(({theme}) => ({
+  color: color.grey['400']
+})) as typeof Link;
 
 const StyledLinkSocialIcon = styled(Link)(({theme}) => ({
-	marginLeft: rem(24),
+  marginLeft: rem(24),
 }))
 
 const StyledTypography = styled(Typography)(({theme}) => ({
-	color: color.grey['400']
-}))
+  color: color.grey['400']
+})) as typeof Typography;
 
 export default function Footer() {
-	// const classes = useStyles();
+  // const classes = useStyles();
 
-	return (
-		<StyledDivRoot aria-label="Site Directory" role="navigation">
-			<MyContainer>
-				<Grid container spacing={0}>
-					<StyledGridItemLogo item xs={12}>
-						<Link component={ReactRouterDom} to="/"><Logo
-							brand="simon-yates" variant="grey-100" width={128}
-						/></Link>
-					</StyledGridItemLogo>
-				</Grid>
+  return (
+    <StyledDivRoot aria-label="Site Directory" role="navigation">
+      <MyContainer>
+        <Grid container spacing={0}>
+          <StyledGridItemLogo item xs={12}>
+            <Link component={ReactRouterDomLink} to="/"><Logo
+              brand="simon-yates" variant="grey-100" width={128}
+            /></Link>
+          </StyledGridItemLogo>
+        </Grid>
 
-				<Grid container spacing={0}>
-					<StyledGridItemLeft item xs={6}>
-						<StyledTypography variant="caption" group="secondary" weight="regular">
-							&copy; 2021 Sybaris Analytics Corporation<br/>
-						</StyledTypography>
-						<StyledTypography variant="caption" group="secondary" weight="regular">
-							{/* @todo mui5 */}
-							<StyledReactRouterDomLink to="/legal">Legal</StyledReactRouterDomLink>
-						</StyledTypography>
-					</StyledGridItemLeft>
-					<StyledGridItemRight item xs={6}>
-						<StyledLinkSocialIcon
-							rel="noopener"
-							title="Find me on LinkedIn"
-							target="_blank"
-							href="https://www.linkedin.com/in/simonyates/"
-						><Logo brand="linkedin" variant="grey-300" width={24}/></StyledLinkSocialIcon>
+        <Grid container spacing={0}>
+          <StyledGridItemLeft item xs={6}>
+            <StyledTypography variant="secondaryCaption">
+              &copy; 2021 Sybaris Analytics Corporation<br/>
+            </StyledTypography>
+            <StyledTypography variant="secondaryCaption">
+              {/* @todo mui5 */}
+              <StyledLink component={ReactRouterDomLink} to="/legal">Legal</StyledLink>
+            </StyledTypography>
+          </StyledGridItemLeft>
+          <StyledGridItemRight item xs={6}>
+            <StyledLinkSocialIcon
+              rel="noopener"
+              title="Find me on LinkedIn"
+              target="_blank"
+              href="https://www.linkedin.com/in/simonyates/"
+            ><Logo brand="linkedin" variant="grey-300" width={24}/></StyledLinkSocialIcon>
 
-						<StyledLinkSocialIcon
-							rel="noopener"
-							title="Follow me on Twitter"
-							target="_blank"
-							href="https://twitter.com/cheftechnical"
-						><Logo brand="twitter" variant="grey-300" width={24}/></StyledLinkSocialIcon>
-					</StyledGridItemRight>
-				</Grid>
-			</MyContainer>
-		</StyledDivRoot>
-	);
+            <StyledLinkSocialIcon
+              rel="noopener"
+              title="Follow me on Twitter"
+              target="_blank"
+              href="https://twitter.com/cheftechnical"
+            ><Logo brand="twitter" variant="grey-300" width={24}/></StyledLinkSocialIcon>
+          </StyledGridItemRight>
+        </Grid>
+      </MyContainer>
+    </StyledDivRoot>
+  );
 };
