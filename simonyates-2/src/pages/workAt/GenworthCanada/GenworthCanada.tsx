661f1d43f8d36cbe2b848a2f3d15254144e93122
Simon Yates <simon@sybaris.ca>
2023-07-29 10:51:09 -0400

    Set the page title

--- simonyates-2/src/pages/workAt/GenworthCanada/GenworthCanada.tsx
+++ simonyates-2/src/pages/workAt/GenworthCanada/GenworthCanada.tsx
@@ -1,23 +1,23 @@
-import DefaultLayout from '../../../layouts/DefaultLayout/DefaultLayout';
 import WorkAtLogo from '../components/WorkAtLogo';
 import WorkAtTemplate from '../components/WorkAtTemplate';
 import Content from './Content';
 import {useSections} from '../../../components/Sections/useSections';
+import PageWrapper from "../../../components/PageWrapper";
 
 export default function GenworthCanada() {
   const [sections, handleSectionChange] = useSections();
 
   return (
-    // <DefaultLayout title="Work at Genworth Canada" top="work">
-    <WorkAtTemplate
-      content={<Content onChange={handleSectionChange}/>}
-      employer="Genworth Canada"
-      logo={<WorkAtLogo brand="genworth-canada" variant="default"/>}
-      role="Developer"
-      sections={sections}
-      when="July 2014 to January 2015"
-      where="Oakville, ON, Canada"
-    />
-    // </DefaultLayout>
+    <PageWrapper title="Work at Genworth Canada">
+      <WorkAtTemplate
+        content={<Content onChange={handleSectionChange}/>}
+        employer="Genworth Canada"
+        logo={<WorkAtLogo brand="genworth-canada" variant="default"/>}
+        role="Developer"
+        sections={sections}
+        when="July 2014 to January 2015"
+        where="Oakville, ON, Canada"
+      />
+    </PageWrapper>
   );
 };
