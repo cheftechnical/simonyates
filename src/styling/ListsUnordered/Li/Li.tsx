9efe2a9be9f5d1eaa933cc2827847095e3f0bbb5
Simon Yates <simon@sybaris.ca>
2023-08-02 15:37:51 -0400

    Cleanup

--- src/styling/ListsUnordered/Li/Li.tsx
+++ src/styling/ListsUnordered/Li/Li.tsx
@@ -10,32 +10,6 @@ interface Props {
   className?: any;
 }
 
-// const useStyles = makeStyles((themeMui) => ({
-// 	li: {
-// 		...themeMui.typography.body1,
-// 		marginBottom: themeMui.spacing(16/8),
-//
-// 		marginLeft: '21px', // this moves the whole line (including bullet)
-// 		paddingLeft: 0,
-//
-// 		listStyle: 'none',
-// 		color: color.grey[900],
-//
-// 		'&:before': {
-// 			content: `'\\2022'`,
-//
-// 			display: 'block',
-// 			position: 'relative',
-// 			maxWidth: '0px',
-// 			maxHeight: '0px',
-// 			left: '-27px', // this moves just the bullet
-// 			top: '-4px',
-// 			color: color.grey['800'],
-// 			fontSize: '20px'
-// 		}
-// 	},
-// }));
-
 const StyledLi = styled('li')(({theme}) => ({
   ...theme.typography.primaryBody,
   marginBottom: theme.spacing(16 / 8),
@@ -61,16 +35,7 @@ const StyledLi = styled('li')(({theme}) => ({
 }))
 
 export default function Li(props: Props) {
-  // const classes = useStyles();
-  const {children, className} = props;
-
-  // const rootClass = (className)
-  // 	? `${classes.li} ${className}`
-  // 	: classes.li
-
-  // return (
-  // 	<li className={rootClass}>{children}</li>
-  // );
+  const {children} = props;
 
   return (
     <StyledLi>{children}</StyledLi>
