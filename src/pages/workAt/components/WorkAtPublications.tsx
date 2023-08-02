62cd6e846bcc292f81c88fffa26458614f900607
Simon Yates <simon@sybaris.ca>
2023-08-02 15:36:48 -0400

    Cleanup

--- src/pages/workAt/components/WorkAtPublications.tsx
+++ src/pages/workAt/components/WorkAtPublications.tsx
@@ -1,4 +1,3 @@
-import rem from '../../../styling/rem';
 import ConditionalIndent from '../../../components/ConditionalIndent/ConditionalIndent';
 import {Box} from "@mui/material";
 
@@ -7,21 +6,8 @@ interface Props {
   next?: boolean;
 }
 
-// const useStyles = makeStyles((themeMui: Theme) => ({
-// 	root: {},
-//
-// 	next: {
-// 		paddingTop: rem(40),
-// 	}
-// }));
-
 export default function WorkAtPublications(props: Props) {
-  // const classes = useStyles();
   const {children, next} = props;
-
-  // const rootClassName = (next)
-  // 	? `${classes.root} ${classes.next}`
-  // 	: classes.root;
 
   return (
     <Box sx={(theme) => ({
