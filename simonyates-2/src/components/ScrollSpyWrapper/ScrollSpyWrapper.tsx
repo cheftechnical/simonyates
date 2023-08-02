839565d0158c36b30a0ced73e3c6dc7d69710ffa
Simon Yates <simon@sybaris.ca>
2023-08-02 11:31:00 -0400

    Paused dev work on ScrollSpy due to architectual challenges

--- simonyates-2/src/components/ScrollSpyWrapper/ScrollSpyWrapper.tsx
+++ simonyates-2/src/components/ScrollSpyWrapper/ScrollSpyWrapper.tsx
@@ -1,5 +1,25 @@
-export default function ScrollSpyWrapper() {
+import {SectionItem} from "../Sections/SectionItem";
+import {Scrollspy} from "@makotot/ghostui";
+import {useMemo, useRef} from "react";
+
+export interface Props {
+  sectionItems: SectionItem[];
+}
+
+export default function ScrollSpyWrapper(props: Props) {
+  const {sectionItems} = props;
+
+  // const sectionRefs = sectionItems.map(() => {
+  //   return useRef<HTMLDivElement>(null);
+  // });
+
   return (
-    <div>[ScrollSpyWrapper]</div>
+    // <Scrollspy sectionRefs={sectionRefs} offset={headerHeight * -1}>
+    //   {({currentElementIndexInViewport}) => (
+        <div>
+          [ScrollSpyWrapper]
+        </div>
+    //   )}
+    // </Scrollspy>
   )
 }
