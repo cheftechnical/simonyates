ad31e18d3857adc10b0fd16f1e2118c22e32582c
Simon Yates <simon@sybaris.ca>
2024-01-27 13:24:41 -0500

    Update root to include theme

--- www/src/routes/root.tsx
+++ www/src/routes/root.tsx
@@ -1,23 +1,30 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
+import ThemeProviderWrapper from '../styling/ThemeProviderWrapper';
+import { Suspense } from 'react';
+import {LinearProgress} from "@mui/material";
 
 
 export function Root() {
   return (
-    <>
-      <header>
-        <Header/>
-      </header>
+    <ThemeProviderWrapper>
+      <Suspense fallback={<LinearProgress />}>
+        <>
+          <header>
+            <Header/>
+          </header>
 
-      <main>
-        <Outlet />
-      </main>
-      
-      <footer>
-        <Footer/>
-      </footer>
-    </>
+          <main>
+            <Outlet />
+          </main>
+          
+          <footer>
+            <Footer/>
+          </footer>
+        </>
+      </Suspense>
+    </ThemeProviderWrapper>
   );
 };
 
