871d00a73b4ce1edfd81c4e4e7321f6ab4d9c342
Simon Yates <simon@sybaris.ca>
2024-02-03 14:38:54 -0500

    Remove testing code

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,18 +1,11 @@
-import { Helmet } from "react-helmet-async";
 import AboutPage from "../pages/AboutPage";
 import DefaultLayout from "../layouts/DefaultLayout";
 
 export function About() {
   return (
-    <>
-      <Helmet>
-        <title>No doout aboot it</title>
-      </Helmet>
-
-      <DefaultLayout>
-        <AboutPage />
-      </DefaultLayout>
-    </>
+    <DefaultLayout>
+      <AboutPage />
+    </DefaultLayout>
   );
 }
 
