c968ff74467e7e1982e378e2638e8de56d5e6eb0
Simon Yates <simon@sybaris.ca>
2025-10-30 19:03:34 -0400

    Migrate EmployerRoleAndWhen component from MUI to Tailwind CSS
    - Remove MUI Link, Typography, and styled dependencies
    - Replace with native h2 element and ReactRouterDom Link directly
    - Convert primaryH3 typography styles to Tailwind classes
    - Apply link styling (text-gray-600, hover:underline)
    - Preserve whitespace-nowrap for time period
    - Add JSDoc comments

--- src/pages/WorkPage/WorkExperience/EmployerRoleAndWhen.tsx
+++ src/pages/WorkPage/WorkExperience/EmployerRoleAndWhen.tsx
@@ -1,60 +1,32 @@
 import { Link as ReactRouterDom } from "react-router-dom";
-import { color } from "../../../styling/Color/Color";
-import { Link, styled, Typography } from "@mui/material";
 
 interface Props {
+  /**
+   * The employer name.
+   */
   employer: string;
+  /**
+   * The link to the employer's work page.
+   */
   href: string;
+  /**
+   * The role performed.
+   */
   role: string;
+  /**
+   * The time period worked.
+   */
   when: string;
 }
-
-// const useStyles = makeStyles((themeMui) => ({
-//   root: {
-//     marginBottom: themeMui.spacing(72 / 8)
-//   },
-//   employer: {
-//     marginBottom: themeMui.spacing(20 / 8)
-//   },
-//   h3Link: {
-//     color: color.grey['600'],
-//     '&:hover': {
-//       color: color.grey['600'],
-//     }
-//   },
-//   readFullDetails: {
-//     marginTop: themeMui.spacing(32 / 8)
-//   },
-//   when: {
-//     whiteSpace: 'nowrap'
-//   }
-// }));
-
-const StyledLink = styled(Link)(() => ({
-  color: color.grey["600"],
-  "&:hover": {
-    color: color.grey["600"],
-  },
-})) as typeof Link;
-
-const StyledSpanWhen = styled("span")(() => ({
-  whiteSpace: "nowrap",
-}));
-
-const StyledTypographyEmployer = styled(Typography)(({ theme }) => ({
-  marginBottom: theme.spacing(20 / 8),
-})) as typeof Typography;
 
 export default function EmployerRollAndWhen(props: Props) {
   const { employer, href, role, when } = props;
 
   return (
-    <StyledTypographyEmployer component="h2" variant="primaryH3">
-      {/*<StyledLink to={href}>*/}
-      <StyledLink component={ReactRouterDom} to={href}>
-        {employer}, {role}, <StyledSpanWhen>{when}</StyledSpanWhen>
-      </StyledLink>
-      {/*</StyledLink>*/}
-    </StyledTypographyEmployer>
+    <h2 className="font-normal text-2xl leading-8 mb-[20px]">
+      <ReactRouterDom to={href} className="text-gray-600 hover:text-gray-600 hover:underline">
+        {employer}, {role}, <span className="whitespace-nowrap">{when}</span>
+      </ReactRouterDom>
+    </h2>
   );
 }
