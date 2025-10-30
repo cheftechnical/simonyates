f759a650abd588205377daf81b193b87d0ec296c
Simon Yates <simon@sybaris.ca>
2025-10-30 12:26:00 -0400

    chore(lint): add eslint-plugin-perfectionist and rules to sort imports, objects, and destructuring; ignore stories

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,40 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
   },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks',
+    'perfectionist',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    // Import and export sorting
+    'perfectionist/sort-imports': ['error', { type: 'natural', order: 'asc' }],
+    'perfectionist/sort-named-imports': ['error', { type: 'natural', order: 'asc' }],
+    'perfectionist/sort-named-exports': ['error', { type: 'natural', order: 'asc' }],
+
+    // Object and destructuring property sorting
+    'perfectionist/sort-objects': ['error', { type: 'natural', order: 'asc' }],
+    'perfectionist/sort-destructuring-properties': ['error', { type: 'natural', order: 'asc' }],
+
+    // Keep existing strictness
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    'src/**/*.stories.*',
+  ],
 };
