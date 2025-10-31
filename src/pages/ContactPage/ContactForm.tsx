c90fd554e8c383dd0d6aee38d1774e9a69c22c3e
Simon Yates <simon@sybaris.ca>
2025-10-31 18:34:32 -0400

    Replace Material-UI SendIcon with custom SVG send icon

--- src/pages/ContactPage/ContactForm.tsx
+++ src/pages/ContactPage/ContactForm.tsx
@@ -2,7 +2,6 @@
  * Resources:
  * https://react-hook-form.com/get-started#IntegratingwithUIlibraries
  */
-import SendIcon from "@mui/icons-material/Send";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { MyButton } from "../../styling/MyButton";
 import { MyTextField2 } from "../../styling/MyTextField2/MyTextField2";
@@ -122,9 +121,25 @@ export function ContactForm(props: Props) {
           }}
         />
 
+        {/* Custom SVG send icon - replaced Material-UI SendIcon */}
         <MyButton
           className="mt-4"
-          endIcon={<SendIcon style={{ fontSize: "16px" }} />}
+          endIcon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+              />
+            </svg>
+          }
           type="submit"
         >
           Send
