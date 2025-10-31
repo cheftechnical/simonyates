c0aff07d452ad9f5dc77262af4c3a9d372ce85d4
Simon Yates <simon@sybaris.ca>
2025-10-31 18:24:07 -0400

    Replace Material-UI ClearIcon with custom SVG and remove unused CenterVertically component

--- src/components/CenterVertically/index.ts
+++
@@ -1,2 +0,0 @@
-import { default as CenterVertically } from "./CenterVertically";
-export default CenterVertically;
--- src/layouts/DefaultLayout/NavTop/ForMobile/Menu.tsx
+++ src/layouts/DefaultLayout/NavTop/ForMobile/Menu.tsx
@@ -2,7 +2,6 @@ import MyContainer from "../../../../styling/MyContainer/MyContainer";
 import { NavLink } from "react-router-dom";
 import Logo from "../../../../components/Logo";
 import { useCallback } from "react";
-import ClearIcon from "@mui/icons-material/Clear";
 import MenuItems from "./MenuItems";
 
 interface Props {
@@ -43,7 +42,21 @@ export default function Menu(props: Props) {
                 onClick={handleClose}
                 className="inline-flex items-center justify-center p-2 text-[#E1E1E1] bg-transparent border-0 cursor-pointer rounded hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
               >
-                <ClearIcon />
+                {/* Custom SVG X icon - replaced Material-UI ClearIcon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
               </button>
             </div>
           </div>
