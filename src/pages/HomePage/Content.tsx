f03bce11bf92d09053484072175ebc1396d7d521
Simon Yates <simon@sybaris.ca>
2025-10-31 15:38:54 -0400

    Convert HomePage Content to Tailwind CSS
    - Replace MUI Typography and Hidden components with Tailwind classes
    - Use responsive Tailwind classes for different viewport sizes
    - Match primaryH1 typography styles (font-normal, text-[54px], leading-[88px], tracking-[0.5px])
    - Remove MUI dependencies from HomePage Content component

--- src/pages/HomePage/Content.tsx
+++ src/pages/HomePage/Content.tsx
@@ -1,44 +1,42 @@
 /**
  * This component will render the welcome text and consider widows/orphans at various viewport sizes.
  */
-// import Typography from '../../styling/Typography/Typography';
 import CenterVertically from "../../components/CenterVertically/CenterVertically";
-import { Hidden, Typography } from "@mui/material";
 
 export default function Content() {
   return (
     <CenterVertically>
-      <Typography component="h1" variant="primaryH1">
-        <Hidden only={[/*'xs',*/ "sm", "md", "lg", "xl"]}>
+      <h1 className="font-normal text-[54px] leading-[88px] tracking-[0.5px]">
+        {/* xs only */}
+        <span className="block sm:hidden">
           Hi, I&rsquo;m Simon&nbsp;Yates.
           <br />I couldn&rsquo;t fit everything into LinkedIn, so I made
           a&nbsp;website.
-          {/*[a]*/}
-        </Hidden>
+        </span>
 
-        <Hidden only={["xs", /*'sm',*/ "md", "lg", "xl"]}>
+        {/* sm only */}
+        <span className="hidden sm:block md:hidden">
           Hi, I&rsquo;m Simon Yates.
           <br />I couldn&rsquo;t fit everything into LinkedIn,
           so&nbsp;I&nbsp;made&nbsp;a&nbsp;website.
-          {/*[b]*/}
-        </Hidden>
+        </span>
 
-        <Hidden only={["xs", "sm", /*'md',*/ "lg", "xl"]}>
+        {/* md only */}
+        <span className="hidden md:block lg:hidden">
           Hi, I&rsquo;m Simon Yates.
           <br />I couldn&rsquo;t fit everything into LinkedIn, so&nbsp;I made
           a&nbsp;website.
-          {/*[c]*/}
-        </Hidden>
+        </span>
 
-        <Hidden only={["xs", "sm", "md" /*'lg', 'xl'*/]}>
+        {/* lg and xl */}
+        <span className="hidden lg:block">
           Hi, I&rsquo;m Simon Yates.
           <br />
           I couldn&rsquo;t fit everything into&nbsp;LinkedIn,
           <br />
           so I made a&nbsp;website.
-          {/*[d]*/}
-        </Hidden>
-      </Typography>
+        </span>
+      </h1>
     </CenterVertically>
   );
 }
