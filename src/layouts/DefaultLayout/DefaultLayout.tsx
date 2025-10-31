1cc588ae6a505a02c460ff0650c5aa138600da99
Simon Yates <simon@sybaris.ca>
2025-10-31 16:07:15 -0400

    Convert DefaultLayout to Tailwind CSS
    - Replace MUI styled components with Tailwind classes
    - Remove MUI styled import and unused rem import
    - Convert StyledDivRoot to div with h-full class
    - Convert StyledMain to main with pt-[162px] class
    - Convert StyledMainFullHeight to conditional mt-[-49px] h-full when fullHeight is true
    - Preserve fullHeight conditional logic for main element and footer margin
    - Simplify footer margin calculation using inline style

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from "react";
-import { styled } from "@mui/material";
 import { ScrollRestoration } from "react-router-dom";
-import rem from "../../styling/rem";
 import NavTop from "./NavTop";
 import Footer from "./Footer";
 import SkipNav from "./SkipNav";
@@ -33,50 +31,40 @@ interface Props {
   top?: string;
 }
 
-const StyledDivRoot = styled("div")(() => ({
-  height: "100%",
-}));
-
-const StyledMain = styled("main")(({ theme }) => ({
-  // marginTop: themeMui.spacing(162 / 8),
-  paddingTop: theme.spacing(162 / 8),
-
-  // color: 'red !important',
-}));
-
-const StyledMainFullHeight = styled(StyledMain)(() => ({
-  marginTop: rem(fullHeightOpticalOffset),
-  height: "100%",
-  // color: 'green !important'
-}));
 
 
 export default function DefaultLayout(props: Props) {
   const { children, fullHeight, top } = props;
 
-  const MyMain = fullHeight ? StyledMainFullHeight : StyledMain;
+  // fullHeightOpticalOffset = -32 - 8 - 8 - 1 = -49px = -3.0625rem
   const footerMarginTop = fullHeight 
-    ? rem(0 - fullHeightOpticalOffset) 
+    ? `${(0 - fullHeightOpticalOffset) / 16}rem` // rem(0 - fullHeightOpticalOffset) = rem(49) = 3.0625rem
     : "88px"; // theme.spacing(88 / 8) = 11 * 8px = 88px
 
   return (
     <>
-      <StyledDivRoot>
+      <div className="h-full">
         <SkipNav mainId={mainId} />
 
         <header>
           <NavTop selected={top} />
         </header>
 
-        {/*/!* IMPORTANT: use `tabIndex=-1` when using the <SkipNav/> component *!/*/}
-        <MyMain id={mainId} tabIndex={-1}>
+        {/* IMPORTANT: use `tabIndex=-1` when using the <SkipNav/> component */}
+        <main
+          id={mainId}
+          tabIndex={-1}
+          className={`pt-[162px] ${
+            fullHeight ? `mt-[-49px] h-full` : ""
+          }`}
+        >
           {children}
-        </MyMain>
+        </main>
 
         <footer className="w-full" style={{ marginTop: footerMarginTop }}>
           <Footer />
         </footer>
-      </StyledDivRoot>
+      </div>
 
       <ScrollRestoration
         getKey={(location) => {
