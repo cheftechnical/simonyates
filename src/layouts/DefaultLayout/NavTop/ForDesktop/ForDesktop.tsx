d46f84d22284f795e67517b3e612ccc797516dca
Simon Yates <simon@sybaris.ca>
2025-10-31 16:11:43 -0400

    Convert NavTop ForDesktop to Tailwind CSS
    - Replace MUI styled components with Tailwind classes
    - Remove MUI imports (Box, Link, styled) and unused color import
    - Convert StyledDivRoot to div with: bg-white pt-12 pb-6 fixed top-0 w-full z-[999]
    - Convert Box components to div elements with Tailwind flex classes
    - Replace MUI Link component with NavLink directly
    - Simplify component structure

--- src/layouts/DefaultLayout/NavTop/ForDesktop/ForDesktop.tsx
+++ src/layouts/DefaultLayout/NavTop/ForDesktop/ForDesktop.tsx
@@ -1,9 +1,7 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../../../../components/Logo";
-import { color } from "../../../../styling/Color/Color";
 import MyContainer from "../../../../styling/MyContainer/MyContainer";
 import MenuItems from "./MenuItems/MenuItems";
-import { Box, Link, styled } from "@mui/material";
 
 interface Props {
   /**
@@ -12,37 +10,26 @@ interface Props {
   selected?: string;
 }
 
-const StyledDivRoot = styled("div")(({ theme }) => ({
-  backgroundColor: color.white,
-  paddingTop: theme.spacing(48 / 8),
-  paddingBottom: theme.spacing(24 / 8),
-  position: "fixed",
-  top: 0,
-  width: "100%",
-  zIndex: 999,
-}));
-
 export default function ForDesktop(props: Props) {
-  // const classes = useStyles();
   const { selected } = props;
 
   return (
-    <StyledDivRoot>
+    <div className="bg-white pt-12 pb-6 fixed top-0 w-full z-[999]">
       <MyContainer>
-        <Box display="flex" justifyContent="flex-end">
+        <div className="flex justify-end">
           {/* Left align */}
-          <Box flexGrow={1}>
-            <Link component={NavLink} to="/">
+          <div className="flex-grow">
+            <NavLink to="/">
               <Logo brand="simon-yates" variant="default" width={149} />
-            </Link>
-          </Box>
+            </NavLink>
+          </div>
 
           {/* Right align */}
-          <Box>
+          <div>
             <MenuItems selected={selected} />
-          </Box>
-        </Box>
+          </div>
+        </div>
       </MyContainer>
-    </StyledDivRoot>
+    </div>
   );
 }
