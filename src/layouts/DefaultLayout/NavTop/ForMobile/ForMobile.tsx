8108fe20450de1dbf8331d9a933707abe7da22a1
Simon Yates <simon@sybaris.ca>
2025-10-31 18:31:44 -0400

    Replace Material-UI MenuIcon with custom SVG hamburger icon

--- src/layouts/DefaultLayout/NavTop/ForMobile/ForMobile.tsx
+++ src/layouts/DefaultLayout/NavTop/ForMobile/ForMobile.tsx
@@ -3,7 +3,6 @@ import { NavLink, useMatches } from "react-router-dom";
 import Logo from "../../../../components/Logo";
 import Menu from "./Menu";
 import { ReactNode, useCallback, useMemo, useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
 
 interface CustomHandles {
   breadcrumb?: ReactNode;
@@ -61,7 +60,21 @@ export default function ForMobile() {
                 onClick={handleShowMenu}
                 className="inline-flex items-center justify-center p-2 text-inherit bg-transparent border-0 cursor-pointer rounded hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300"
               >
-                <MenuIcon />
+                {/* Custom SVG hamburger menu icon - replaced Material-UI MenuIcon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
               </button>
             </div>
           </div>
