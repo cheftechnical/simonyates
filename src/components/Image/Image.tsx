fc18eee73adda24c66ae2bf80b0cedd92b34afec
Simon Yates <simon@sybaris.ca>
2025-10-31 18:16:45 -0400

    Migrate Image component to Tailwind CSS

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,32 +1,16 @@
-import rem from "../../styling/rem";
-import { styled } from "@mui/material";
-
 interface Props {
   alt: string;
-  className?: any;
+  className?: string;
   src: string;
 }
 
-// @todo mui5
-// const useStyles = makeStyles((themeMui) => ({
-// 	root: {
-// 		marginBottom: rem(-4),
-// 		width: '100%'
-// 	}
-// }));
-
-const StyledImg = styled("img")(() => ({
-  marginBottom: rem(-4),
-  width: "100%",
-}));
-
 export default function Image(props: Props) {
-  const { alt, className, src } = props;
+  const { alt, className = "", src } = props;
 
   return (
-    <StyledImg
+    <img
       alt={alt}
-      className={`${className}`}
+      className={`-mb-1 w-full ${className}`}
       // src={process.env.PUBLIC_URL + src} @todo <!---- this needs to be set
       src={src}
     />
