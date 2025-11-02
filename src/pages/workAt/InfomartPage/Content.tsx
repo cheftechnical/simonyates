3e7ef51d678cc655935d6ea0faba2276593abc78
Simon Yates <simon@sybaris.ca>
2025-11-02 12:38:58 -0500

    Migrate InfomartPage content from Material-UI Typography and styled to Tailwind CSS

--- src/pages/workAt/InfomartPage/Content.tsx
+++ src/pages/workAt/InfomartPage/Content.tsx
@@ -2,12 +2,7 @@ import WorkAtDescription from "../components/WorkAtDescription";
 import Callout from "../../../components/Callout/Callout";
 import WorkAtSectionTitle from "../components/WorkAtSectionTitle";
 import WorkAtProject from "../components/WorkAtProject";
-import { styled, Typography } from "@mui/material";
 import { sectionCollection } from "./sectionCollection.ts";
-
-const StyledCallout = styled(Callout)(({ theme }) => ({
-  marginBottom: theme.spacing(20 / 8),
-}));
 
 export default function Content() {
   return (
@@ -17,33 +12,31 @@ export default function Content() {
           Infomart was a division of Postmedia, the industry standard for
           Canadian newspaper&nbsp;archiving.
         </WorkAtDescription>
-        {/*<Callout className={classes.callout}>*/}
-        <StyledCallout>
+        <Callout className="mb-5">
           <strong>Fun Fact: </strong>Elasticsearch was still in early beta when
           I started working with it on this project. At the time, the latest
           version was version&nbsp;~&nbsp;0.19–0.2.x
-          {/*</Callout>*/}
-        </StyledCallout>
+        </Callout>
       </div>
 
       <div id={sectionCollection.aboutTheProject.id}>
         <WorkAtSectionTitle>About the Project</WorkAtSectionTitle>
-        <Typography component="p" variant="primaryBody">
+        <p className="font-primary font-normal text-base leading-6 tracking-[0.5px]">
           For the last 30 years, Infomart had collected over 200 million
           newspaper articles from every major Canadian&nbsp;publisher.
-        </Typography>
-        <Typography component="p" variant="primaryBodyNext">
+        </p>
+        <p className="font-primary font-normal text-base leading-6 tracking-[0.5px] mt-6">
           The goal of this project was to enhance the search capabilities of the
           tool, while expanding its archives to include content from leading
           social media platforms like Twitter, Facebook, and&nbsp;YouTube.
-        </Typography>
-        <Typography component="p" variant="primaryBodyNext">
+        </p>
+        <p className="font-primary font-normal text-base leading-6 tracking-[0.5px] mt-6">
           Data was originally stored in Microsoft SQL Server, but given the full
           text requirements of the tool, Elasticsearch proved to be an ideal
           use-case for this modernization effort. Elasticsearch was still in its
           early beta then (~ version 0.19–0.2.x), so the documentation was a
           challenge, but the promise was very&nbsp;high.
-        </Typography>
+        </p>
 
         <WorkAtProject next name="Languages & Frameworks" />
         <Callout
