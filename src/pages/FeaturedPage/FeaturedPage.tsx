6efff01d600568bbbe419b219f49a35cdb941470
Simon Yates <simon@sybaris.ca>
2025-11-05 17:30:05 -0500

    Fix event order in FeaturedPage

--- src/pages/FeaturedPage/FeaturedPage.tsx
+++ src/pages/FeaturedPage/FeaturedPage.tsx
@@ -38,7 +38,42 @@ export default function Featured() {
                 {/*</SectionDescription>*/}
 
                 <FeaturedEvents>
-                <FeaturedEvent
+                  <FeaturedEvent
+                    title="Starting and Building a Career in Software Engineering"
+                    host="BrainStation Toronto"
+                    when="September 17, 2024"
+                  >
+                    <div className="mt-4">
+                      <a
+                        href="https://www.linkedin.com/events/startingandbuildingacareerinsof7240068930323587072/"
+                        className="text-blue-500 no-underline hover:underline inline-flex items-center"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        LinkedIn Event
+                        <svg
+                          className="ml-1 h-4 w-4"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                          aria-hidden="true"
+                        >
+                          <path
+                            d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6M15 3h6v6M10 14L21 3"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </a>
+                    </div>
+                  </FeaturedEvent>
+
+                  {/* ------------ */}
+
+                  <FeaturedEvent
+                    next
                     title="Starting and Building a Career in Software Engineering"
                     host="BrainStation Toronto"
                     when="August 22, 2024"
@@ -72,40 +107,6 @@ export default function Featured() {
 
                   {/* ------------ */}
 
-                  <FeaturedEvent
-                    next
-                    title="Starting and Building a Career in Software Engineering"
-                    host="BrainStation Toronto"
-                    when="September 17, 2024"
-                  >
-                    <div className="mt-4">
-                      <a
-                        href="https://www.linkedin.com/events/startingandbuildingacareerinsof7240068930323587072/"
-                        className="text-blue-500 no-underline hover:underline inline-flex items-center"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        LinkedIn Event
-                        <svg
-                          className="ml-1 h-4 w-4"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6M15 3h6v6M10 14L21 3"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </a>
-                    </div>
-                  </FeaturedEvent>
-
-                  {/* ------------ */}
                   <FeaturedEvent
                     next
                     title="Starting and Building a Career in Tech"
