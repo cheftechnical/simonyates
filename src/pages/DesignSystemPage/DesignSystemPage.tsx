f90532a45e0ee0abfd25ad421a15442943b7e35c
Simon Yates <simon@sybaris.ca>
2025-12-26 19:31:41 -0500

    Update paragraph spacing to mt-6 in DesignSystem page for consistency

--- src/pages/DesignSystemPage/DesignSystemPage.tsx
+++ src/pages/DesignSystemPage/DesignSystemPage.tsx
@@ -36,35 +36,35 @@ export function DesignSystem() {
               To begin, I had to decide on a scale that resonated with me. I wanted the information hierarchy to be easy to navigate. Too many levels and the user would get lost; too few, and there wouldn&rsquo;t be sufficient granularity to structure the&nbsp;data.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               Triangulating with a 3-level hierarchy felt like a perfect balance, so I settled on&nbsp;that.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               After exploring several ratios and proportions, I settled on a pure tritonic (also called &ldquo;tritonic modular&rdquo;) scale for text, and the Fibonacci sequence for mostly everything&nbsp;else.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               The tritonic scale is derived by dividing an octave (2:1) into 3 equal steps. It means 3 steps up, and the base size doubles (visual octave), while 3 steps down halves the base size (visual&nbsp;sub-octave).
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               My design system includes both digital and print media, each with its own set of&nbsp;constraints.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               To establish my type scale, I first had to decide on my base font&nbsp;size.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               For digital, a popular size is 16 px. I saw no objection and agreed to use the&nbsp;same.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               For print, a standard type size is 12 pt. Again, I saw no objection and agreed to use the&nbsp;same.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               I then calculated a scale for each media base using the tritonic scale and this&nbsp;formula:
             </p>
 
@@ -72,60 +72,60 @@ export function DesignSystem() {
               <code>ratioPerStep = ∛2 ≅ 1.259921</code>
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               For each step, we assign a system-wide name.
             </p>
 
             <ul className="text-[1.0rem] leading-normal">
-              <li className="list-disc list-inside mt-4">For each increment above our base: &ldquo;step-a#&rdquo;</li>
-              <li className="list-disc list-inside mt-4">For each decrement below our base: &ldquo;step-z#&rdquo;</li>
+              <li className="list-disc list-inside mt-6">For each increment above our base: &ldquo;step-a#&rdquo;</li>
+              <li className="list-disc list-inside mt-6">For each decrement below our base: &ldquo;step-z#&rdquo;</li>
             </ul>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               This naming convention scales indefinitely in both directions, but the media naturally enforces constraints. Unless I was making a billboard to hang off the side of a building, it wouldn&rsquo;t be practical to use a &ldquo;step-a100&rdquo;, for example. That said, I would likely establish a different base for such a use case, but you get the idea.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               Additionally, we know that the minimum readable text size on a modern digital device is 12 px, so there is no point in computing anything smaller than that.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               Since we&rsquo;ve already decided that 16 px will be our base size, we can set it to 1 rem and label the next 3 parents as h3, h2, and h1 relative to&nbsp;it.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               For sizes smaller than our base, we can label them small, extra small, and so on, but since we have already set our floor at 12 px, our system doesn&rsquo;t support anything smaller than a &ldquo;small&rdquo;, so that&rsquo;s all we&rsquo;ll&nbsp;include.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               The benefit of this system is that it decouples the HTML elements (e.g., h1, h2, h3, body) and the programming/implementation concept from our design&nbsp;scale.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               We are also not limited to the maximum size of whatever h1 is set to. This gives us the freedom to use larger text sizes without forcing exceptions. We can expand the system at any time to include larger font sizes without breaking our naming&nbsp;pattern.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               A great example of this is with 404&nbsp;pages.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               It is not uncommon for the &ldquo;404&rdquo; to be printed very large. The number itself is the primary content of the page and is therefore displayed in a font size typically much larger than a standard h1 HTML element. To achieve this, however, the design would need to specify an exception if they are relying on a conventional h1 name within the design tool, even though it may in fact be coded as an HTML h1 element; the treatment would&nbsp;differ.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               The &ldquo;Step&rdquo; system avoids this entirely. The designer picks the font size best suited to their use case, and the engineer decides which HTML element is appropriate. Each person on the project can focus on the core skills they are most familiar&nbsp;with.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               Likewise, deciding how a proportional subhierarchy should appear &ndash; for example, &ldquo;Page Not Found&rdquo; and &ldquo;Here are some helpful links to help you find what you were looking for&rdquo; &ndash; is already scaled and&nbsp;proportioned.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               In the following table, I&rsquo;ve listed 3 visual octaves and 1 visual sub-octave. The standard h1&ndash;3, body, and small body sizes have been labelled and highlighted to show where they sit within the overall&nbsp;scale.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               My 404 page uses 2 visual octaves higher for its messaging.
             </p>
         
@@ -254,23 +254,23 @@ export function DesignSystem() {
               Luminance Scale
             </h2>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               For colour, I wanted to start with a balanced grayscale system before introducing&nbsp;hues.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               Many digital colouring systems try to capture all available luminance and hues into equally spaced samples, but that&rsquo;s not how our human eye perceives light. Our eyes are more sensitive to some types of light than&nbsp;others.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               After exploring 6 methods to create a simplified 5-value system, I settled on #757575 as my middle gray. Thankfully, when this shade is used to render text on a white background, it is also AA accessible, so that made the rest of my work much&nbsp;easier.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               For digital, I felt black (#000000) was too harsh and contrasty for my taste, so I chose a very dark gray (#222222) to use as a soft black. This became my dark&nbsp;anchor.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               I then computed a stepped scale that crossed over middle gray (#757575) and terminated at our soft black (#222222) using the OKLab L-spacing colour system. This produced a scale that accounts for biases in the human eye&rsquo;s perception of&nbsp;light.
             </p>
 
@@ -294,13 +294,13 @@ export function DesignSystem() {
             </p>
 
             <ul className="text-[1.0rem] leading-normal">
-              <li className="list-disc list-inside mt-4">The lightest shade of gray acts as a soft white</li>
-              <li className="list-disc list-inside mt-4">The darkest shade of gray acts as a soft black</li>
-              <li className="list-disc list-inside mt-4">Text in grays below middle gray (&le; 400) works beautifully on soft black (#222222) backgrounds</li>
-              <li className="list-disc list-inside mt-4">500 is middle gray (#757575)</li>
-              <li className="list-disc list-inside mt-4">Text in grays darker than middle gray (&ge; 500) works on white (#FFFFFF) backgrounds</li>
-              <li className="list-disc list-inside mt-4">Gray-25 on soft black (#222222) appears as bright white</li>
-              <li className="list-disc list-inside mt-4">Gray-50 on soft black (#222222) appears as white</li>
+              <li className="list-disc list-inside mt-6">The lightest shade of gray acts as a soft white</li>
+              <li className="list-disc list-inside mt-6">The darkest shade of gray acts as a soft black</li>
+              <li className="list-disc list-inside mt-6">Text in grays below middle gray (&le; 400) works beautifully on soft black (#222222) backgrounds</li>
+              <li className="list-disc list-inside mt-6">500 is middle gray (#757575)</li>
+              <li className="list-disc list-inside mt-6">Text in grays darker than middle gray (&ge; 500) works on white (#FFFFFF) backgrounds</li>
+              <li className="list-disc list-inside mt-6">Gray-25 on soft black (#222222) appears as bright white</li>
+              <li className="list-disc list-inside mt-6">Gray-50 on soft black (#222222) appears as white</li>
             </ul>            
 
             <table className="border-gray-700 border-2 mt-8 w-full">
@@ -410,7 +410,7 @@ export function DesignSystem() {
               For print, I take a more contrasty approach. Blacks are <em>black</em>, whites are crisp, and middle gray is rarely used. No other semi-tones exist. Instead, line weights and negative space are used strategically to create&nbsp;depth.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               Why? Black ink is cheaper, and grays on my laser printer can look washed out. And it looks&nbsp;cool!
             </p>
 
@@ -473,11 +473,11 @@ export function DesignSystem() {
               There are several ways this scale can be used, depending on how I want to present the information. Again, the goal of this system is to articulate and conceptualize broader themes while not limiting creative flexibility. An overly strict system would be too &ldquo;templaty&rdquo;, and that&rsquo;s not&nbsp;interesting.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               These levels can either be used as a multiplier of the current font size to determine the top margin, or the number of shades, steps or visual octaves from the current text to use. Both methods produce different results, and both are&nbsp;valid.
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               In the case of shades, one extra rule is added,&nbsp;however:
             </p>
 
@@ -485,18 +485,18 @@ export function DesignSystem() {
               <em>&ldquo;No two shade levels should appear next to each&nbsp;other.&rdquo;</em>
             </p>
 
-            <p className="text-[1.0rem] leading-normal mt-4">
+            <p className="text-[1.0rem] leading-normal mt-6">
               There is not enough visual contrast within the scale to perceive the difference without it appearing as an error. When counting shade levels, always skip the first&nbsp;one.
             </p>
 
             <ul className="text-[1.0rem] leading-normal mt-8" style={{ paddingLeft: '2rem' }}>
-              <li className="list-disc mt-4">
+              <li className="list-disc mt-6">
                 <strong>Example #1:</strong> This copy is written in soft black (#222222), and the above table is dark gray (#464646). The table has a &ldquo;Close&rdquo; proximity to the copy &ndash; but it&rsquo;s not the copy, so it needs visual separation. Using the Luminance Scale, we start at gray-900 (#222222), skip the first shade (#333333), then one level up to gray-700 (#464646) &ndash; the exact colour of the table border and header&nbsp;background.
               </li>
-              <li className="list-disc mt-4">
+              <li className="list-disc mt-6">
                 <strong>Example #2:</strong> This copy (#222222) has a discrete separation from the side navigation. It&rsquo;s a shift in cognitive processing from reading this text to navigating. Starting from gray-900 (#222222), skipping the first value, then up 3 levels gives us gray-500 (#757575) &ndash; the exact colour of our navigation&nbsp;text.
               </li>
-              <li className="list-disc mt-4">
+              <li className="list-disc mt-6">
                 <strong>Example #3:</strong> The Typographic, Luminance, and Proximity Scale headings are all discrete topics within a broader theme. They could be referenced independently. Each of those headings has a top margin that is 3x the font size of the heading, and each subsequent paragraph has a top margin that is 1x the font size to show that it is closely related to that&nbsp;heading.
               </li>
             </ul>
