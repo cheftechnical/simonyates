2ad9279e4c7124af083e8d9a2b3c9014f3e74c39
Simon Yates <simon@sybaris.ca>
2025-12-27 02:49:48 -0500

    Update Work hero copy
    Adjust headline copy, keep responsive line break, and prevent orphan with nbsp.

--- src/page-components/WorkPage/Hero/Hero.tsx
+++ src/page-components/WorkPage/Hero/Hero.tsx
@@ -1,9 +1,8 @@
 export default function Hero() {
   return (
     <h1 className="font-normal text-2xl leading-relaxed tracking-[0.25px]">
-      Here is a glimpse of&nbsp;my&nbsp;life, <br className="hidden lg:block" />mostly&nbsp;as a&nbsp;programmer.<br/>
-      {/* <span className="hidden md:block">md</span> */}
-      {/* <span className="hidden lg:block">lg</span> */}
+      Here is a glimpse of my life, <br className="hidden lg:block" />
+      told through the work I’ve&nbsp;done.
     </h1>
   );
 }
