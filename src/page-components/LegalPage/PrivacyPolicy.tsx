e54edcb6acfc49a82841103871f6da7d01b25695
Simon Yates <simon@sybaris.ca>
2025-12-27 11:28:22 -0500

    Update Privacy Policy

--- src/page-components/LegalPage/PrivacyPolicy.tsx
+++ src/page-components/LegalPage/PrivacyPolicy.tsx
@@ -2,16 +2,14 @@ import Ul from "../../styling/ListsUnordered/Ul/Ul";
 import Li from "../../styling/ListsUnordered/Li/Li";
 
 const legalName = "Sybaris Analytics Corporation";
-const tradeName = "Sybaris";
 const domainName = "simonyates.ca";
-const address = "";
 
 export type PrivacyPolicyProps = {
   /**
    * The root ID
    */
   id: string;
-}
+};
 
 export function PrivacyPolicy(props: PrivacyPolicyProps) {
   const { id } = props;
@@ -23,127 +21,73 @@ export function PrivacyPolicy(props: PrivacyPolicyProps) {
       </h2>
 
       <p className="font-normal text-base leading-normal mt-4">
-        This Privacy Policy applies to all of the products, services and
-        websites of {legalName}, or its subsidiaries or
-        affiliated&nbsp;companies.
+        This Privacy Policy applies to the website {domainName}, operated by{" "}
+        {legalName}.
       </p>
 
       <p className="font-normal text-base leading-normal mt-4">
-        If you have any questions about this Privacy Policy, please feel free to
-        contact us trough our website, or write to us&nbsp;at:
+        If you have any questions about this Privacy Policy, you may contact us
+        through the website.
       </p>
 
-      <address>
-        <p className="font-normal text-base leading-normal mt-6">
-          Privacy Matters
-          <br />
-          c/o {legalName}
-          <br />
-          {address}
-        </p>
-      </address>
-
       <h3 className="font-normal text-xl leading-8 mt-[calc(1.2599rem*3)]">
-        Information we collect, and how we use&nbsp;it:
+        Information We Collect, and How We Use It
       </h3>
 
       <p className="font-normal text-base leading-normal mt-6">
-        We may collect the following types of&nbsp;information:
+        We collect very limited information, only to operate and understand the
+        basic use of the site.
+      </p>
+
+      <h4 className="font-normal text-lg leading-8 mt-6">
+        Information you provide
+      </h4>
+
+      <p className="font-normal text-base leading-normal mt-4">
+        If you contact us through the website or by email, we will receive the
+        information you choose to provide (such as your name and email address).
+        We use this information only to respond to your message and do not use
+        it for marketing purposes.
+      </p>
+
+      <h4 className="font-normal text-lg leading-8 mt-6">
+        Analytics and log information
+      </h4>
+
+      <p className="font-normal text-base leading-normal mt-4">
+        We use Vercel Web Analytics to understand basic, aggregate usage of the
+        site, such as:
       </p>
 
       <Ul next>
-        <Li>
-          <strong>Information you provide</strong> &mdash; When you sign up for
-          a {tradeName} Account, we ask you for personal information. We may
-          combine the information you submit under your account with information
-          from other {tradeName} services or third parties in order to provide
-          you with a better experience and improve the quality of our services.
-          For certain services, we may give you the opportunity to opt of
-          combining such information. You can use the {domainName} Dashboard to
-          learn more about the information associated with your&nbsp;Account.
-        </Li>
-
-        <Li>
-          <strong>Cookies</strong> &mdash; When you visit {domainName}, we send
-          one or more cookies to your computer or other device. We use cookies
-          to improve the quality of our services, including for storing
-          user&nbsp;preferences.
-        </Li>
-
-        <Li>
-          <strong>Log information</strong> &mdash; When you access {tradeName}{" "}
-          services via a browser, application or other client, our services
-          automatically record certain information. These server logs may
-          include information such as your web request, your interaction with a
-          service, Internet Protocol address, browser type, browser language,
-          the date and time of your request, and one or more cookies that may be
-          uniquely identify your browser or your&nbsp;account.
-        </Li>
-
-        <Li>
-          <strong>User communications</strong> &mdash; When you send email or
-          other communications to
-          {tradeName}, we may retain those communications in order to process
-          your inquiries, respond to your requests and improve our services.
-          When you send and receive SMS to or from one of services that provides
-          SMS functionality, we may collect information associated with those
-          messages, such as the phone number, the wireless carrier associated
-          with the phone number, the content of the message, and the date and
-          time of the transaction. We may use your email address to communicate
-          with you about our&nbsp;services.
-        </Li>
-
-        <Li>
-          <strong>Affiliated {legalName} Services on other sites</strong>{" "}
-          &mdash; We offer some of our services on or through other web sites.
-          Personal information that you provide to those sites may be sent to{" "}
-          {legalName} in order to deliver the service. We process such
-          information under this Privacy&nbsp;Policy.
-        </Li>
-
-        <Li>
-          <strong>Third Party Applications</strong> &mdash; {legalName} may make
-          available third party applications, such as gadgets or extensions,
-          through its services. The information collected by
-          {legalName} when you enable a third party application is processed
-          under this Privacy Policy. Information collected by the third party
-          application provider is governed by their privacy&nbsp;policies.
-        </Li>
-
-        <Li>
-          <strong>Other sites</strong> &mdash; This Privacy Policy applies to{" "}
-          {legalName} services only. We do not exercise control over other sites
-          that include {tradeName} applications, products or services, or links
-          from within our various services. These other sites may place their
-          own cookies or other files on your computer, collect data or solicit
-          personal information from&nbsp;you.
-        </Li>
+        <Li>pages visited</Li>
+        <Li>referring sites</Li>
+        <Li>general geographic region</Li>
       </Ul>
 
       <p className="font-normal text-base leading-normal mt-6">
-        In addition to the above, we may use the information we collect&nbsp;to:
+        This analytics data:
       </p>
 
       <Ul next>
-        <Li>
-          Provide, maintain, protect and improve our services (including
-          advertising services) and develop new services;&nbsp;and
-        </Li>
-        <Li>
-          Protect the rights or property of {legalName} or our&nbsp;users.
-        </Li>
+        <Li>does not use cookies</Li>
+        <Li>does not use third-party advertising identifiers</Li>
+        <Li>is collected in an anonymized, aggregated form</Li>
+        <Li>is used solely to understand site usage and improve performance</Li>
       </Ul>
 
-      <p className="font-normal text-base leading-normal">
-        If we use this information in a manner different than the purpose for
-        which it was collected, then we will ask for your consent prior to
-        such&nbsp;use.
+      <p className="font-normal text-base leading-normal mt-6">
+        Like most websites, our hosting provider may also collect standard server
+        log information (such as IP address, browser type, and request time) for
+        security, reliability, and performance monitoring.
       </p>
 
+      <h3 className="font-normal text-xl leading-8 mt-[calc(1.2599rem*3)]">
+        Cookies
+      </h3>
+
       <p className="font-normal text-base leading-normal mt-6">
-        {tradeName} process personal information on our services in the Canada
-        and other countries. In some cases, we process personal information out
-        of your own&nbsp;country.
+        This website does not use cookies.
       </p>
 
       <h3 className="font-normal text-xl leading-8 mt-[calc(1.2599rem*3)]">
@@ -151,43 +95,21 @@ export function PrivacyPolicy(props: PrivacyPolicyProps) {
       </h3>
 
       <p className="font-normal text-base leading-normal mt-6">
-        {legalName} only shares personal information with other companies or
-        individuals outside of {legalName}
-        in the following limited&nbsp;circumstances:
+        We do not sell, rent, or trade personal information.
+      </p>
+
+      <p className="font-normal text-base leading-normal mt-6">
+        Information may be shared only in the following limited circumstances:
       </p>
 
       <Ul next>
         <Li>
-          We have your consent. We require opt-in consent for the sharing of any
-          sensitive personal&nbsp;information.
+          with service providers (such as hosting and analytics) strictly for
+          operating the site
         </Li>
-
+        <Li>when required to comply with applicable law or legal process</Li>
         <Li>
-          We provide such information to our subsidiaries, affiliated companies
-          or other trusted businesses or persons for the purpose of processing
-          personal information on our behalf. We require that these parties
-          agree to process such information based on our instructions and in
-          compliance with this Privacy Policy and other appropriate
-          confidentiality and security&nbsp;measures.
-        </Li>
-
-        <Li>
-          We have a good faith belief that access, use, and preservation or
-          disclosure of such information is reasonably necessary to (a) satisfy
-          any applicable law, regulation, legal process or enforceable
-          government request, (b) enforce applicable Terms of Service, including
-          investigation of potential violations thereof, (c) detect, prevent, or
-          otherwise address fraud, security or technical issues, or (d) protect
-          against harm to the rights, property or safety of {legalName}, its
-          users or the public as required or permitted by&nbsp;law.
-        </Li>
-
-        <Li>
-          If {legalName} becomes involved in a merger, acquisition, or any form
-          of sale of some or all of its assets, we will ensure the
-          confidentiality of any personal information involved in such
-          transactions and provide notice before personal information is
-          transferred and becomes subject to a a different privacy&nbsp;policy.
+          to protect the rights, property, or safety of {legalName} or others
         </Li>
       </Ul>
 
@@ -196,76 +118,35 @@ export function PrivacyPolicy(props: PrivacyPolicyProps) {
       </h3>
 
       <p className="font-normal text-base leading-normal mt-6">
-        We take appropriate security measures to protect against unauthorized
-        access to, or unauthorized alteration, disclosure or destruction of
-        data. These include internal reviews of our data collection, storage and
-        processing practices and security measures, including appropriate
-        encryption and physical security measures to guard against unauthorized
-        access to systems where we store personal&nbsp;data.
-      </p>
-
-      <p className="font-normal text-base leading-normal mt-6">
-        We restrict access to personal information to {legalName} employees,
-        contractors and agents who need to know that information in order to
-        process it on our behalf. These individuals are bound by confidentiality
-        obligations and may be subject to discipline, including termination and
-        criminal prosecution, if they fail to meet these&nbsp;obligations.
+        We take reasonable measures to protect information against unauthorized
+        access, disclosure, or destruction. Access to information is limited to
+        those who need it for site operation or correspondence.
       </p>
 
       <h3 className="font-normal text-xl leading-8 mt-[calc(1.2599rem*3)]">
-        Accessing and updating personal&nbsp;information
+        Accessing and Updating Information
       </h3>
 
       <p className="font-normal text-base leading-normal mt-6">
-        When you use {tradeName} services, we make good faith efforts to provide
-        you with access to your personal information and either correct this
-        data if it is inaccurate or delete such data at your request if it is
-        not otherwise required to be retained by law or for legitimate business
-        purposes. We ask individual users to identify themselves and the
-        information requested to be accessed, corrected or removed before
-        processing such requests, and we may decline to process requests that
-        are unreasonably repetitive or systematic, require disproportionate
-        technical effort, jeopardize the privacy of others, or would be
-        extremely impractical (for instance, requests concerning information
-        residing on backup archives), or for which access is not otherwise
-        required. In any case where we provide information access and
-        correction, we perform this services free of charge, except if doing so
-        would require a disproportionate effort. Because of the way we maintain
-        certain services, after you delete your information, residual copies
-        make take a period of time before they are deleted from our active
-        servers and may remain in our backup systems. Please review the services
-        Help Centers for more&nbsp;information.
+        If you have contacted us and wish to request correction or deletion of
+        your information, you may do so by contacting us through the website. We
+        will make reasonable efforts to comply, subject to legal and operational
+        requirements.
       </p>
 
       <h3 className="font-normal text-xl leading-8 mt-[calc(1.2599rem*3)]">
-        Enforcement
+        Changes to This Privacy Policy
       </h3>
 
       <p className="font-normal text-base leading-normal mt-6">
-        {tradeName} regularly reviews its compliance with this Privacy Policy.
-        When we receive formal written complaints, it is {tradeName}&rsquo;s
-        policy to contact the complaining user regarding his or her concerns. We
-        will cooperate with the appropriate regulatory authorities, including
-        local data protection authorities, to resolve any complains regarding
-        the transfer of personal data that cannot be resolved between{" "}
-        {legalName} and an&nbsp;individual.
-      </p>
-
-      <h3 className="font-normal text-xl leading-8 mt-[calc(1.2599rem*3)]">
-        Changes to this Privacy&nbsp;Policy
-      </h3>
-
-      <p className="font-normal text-base leading-normal mt-6">
-        Please note that this Privacy Policy may change from time to time. We
-        will not reduce your rights under this Privacy Policy without your
-        explicit consent. We will post any Privacy Policy changes on this page
-        and, if the changes are significant, we will provide a more prominent
-        notice (including, for certain services, email notification of Privacy
-        Policy changes). We will also keep prior versions of this Privacy Policy
-        in an archive for your&nbsp;review.
+        This Privacy Policy may be updated from time to time. Any changes will
+        be posted on this page. Changes will not reduce your rights without
+        notice.
       </p>
     </div>
   );
 }
 
 export default PrivacyPolicy;
+
+
