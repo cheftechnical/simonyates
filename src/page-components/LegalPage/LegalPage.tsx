8d47e42adec4d4fd154193603c42ca4c4f985c3c
Simon Yates <simon@sybaris.ca>
2025-12-28 11:16:32 -0500

    Remove PageWrapper from Legal page
    PageWrapper is deprecated/no-op; remove wrapper from LegalPage component.

--- src/page-components/LegalPage/LegalPage.tsx
+++ src/page-components/LegalPage/LegalPage.tsx
@@ -1,5 +1,4 @@
 import NavRight from "../../components/NavRight/NavRight";
-import PageWrapper from "../../components/PageWrapper";
 import MyContainer from "../../styling/MyContainer/MyContainer";
 import { Sections } from "../../types/Sections";
 import PrivacyPolicy from "./PrivacyPolicy";
@@ -18,23 +17,21 @@ const sections: Sections = {
 
 export function Legal() {
   return (
-    <PageWrapper title="Legal">
-      <MyContainer>
-        <div className="flex flex-wrap">
-          {/* Content */}
-          <div className="w-full md:w-9/12 md:pr-48">
-            <h1 className="sr-only">Legal</h1>
-            <PrivacyPolicy id={sections.privacyPolicy.id} />
-            <TermsOfService id={sections.termsOfService.id} />
-          </div>
-
-          {/* Navigation */}
-          <div className="hidden md:block md:w-3/12">
-            <NavRight sections={sections} />
-          </div>
+    <MyContainer>
+      <div className="flex flex-wrap">
+        {/* Content */}
+        <div className="w-full md:w-9/12 md:pr-48">
+          <h1 className="sr-only">Legal</h1>
+          <PrivacyPolicy id={sections.privacyPolicy.id} />
+          <TermsOfService id={sections.termsOfService.id} />
         </div>
-      </MyContainer>
-    </PageWrapper>
+
+        {/* Navigation */}
+        <div className="hidden md:block md:w-3/12">
+          <NavRight sections={sections} />
+        </div>
+      </div>
+    </MyContainer>
   );
 }
 
