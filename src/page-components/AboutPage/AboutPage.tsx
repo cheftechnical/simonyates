89a14b1ace1b061f3ce1fa29233913d83dfe8a3d
Simon Yates <simon@sybaris.ca>
2025-12-28 02:12:51 -0500

    Remove PageWrapper from About page
    PageWrapper is a no-op in Next; remove it from AboutPage.

--- src/page-components/AboutPage/AboutPage.tsx
+++ src/page-components/AboutPage/AboutPage.tsx
@@ -1,17 +1,14 @@
-import PageWrapper from "../../components/PageWrapper";
 import Hero from "./Hero/Hero";
 import LifeAsAMaker from "./LifeAsAMaker/LifeAsAMaker";
 import Stats from "./Stats/Stats";
 
 export function AboutPage() {
   return (
-    <PageWrapper title="About">
-      <article>
-        <Hero />
-        <Stats />
-        <LifeAsAMaker />
-      </article>
-    </PageWrapper>
+    <article>
+      <Hero />
+      <Stats />
+      <LifeAsAMaker />
+    </article>
   );
 }
 
