9a66b813dd3c4173aef27b5301c7f9959f8b0840
Simon Yates <simon@sybaris.ca>
2025-12-28 12:20:48 -0500

    Remove PageWrapper from Figpie Trigonometry page
    PageWrapper is deprecated/no-op; remove wrapper from FigpieTrigonometryPage component.

--- src/page-components/FigpieTrigonometryPage/FigpieTrigonometryPage.tsx
+++ src/page-components/FigpieTrigonometryPage/FigpieTrigonometryPage.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import PageWrapper from "../../components/PageWrapper";
 import MyContainer from "../../styling/MyContainer/MyContainer";
 import MathJaxProviderWrapper from "./MathJaxProviderWrapper";
 import { CircleBasicsGridContainer } from "./sections/CircleBasicsGridContainer/CircleBasicsGridContainer";
@@ -10,28 +9,26 @@ import RotationGridContainer from "./sections/RotationGridContainer";
 export default function FigpieTrigonometryPage() {
   return (
     <MathJaxProviderWrapper>
-      <PageWrapper title="How it Works">
-        <MyContainer>
-          <h1 className="font-primary font-normal text-4xl leading-relaxed">
-            How to Build a Pie Chart With Cubic B&eacute;zier&nbsp;Curves
-          </h1>
+      <MyContainer>
+        <h1 className="font-primary font-normal text-4xl leading-relaxed">
+          How to Build a Pie Chart With Cubic B&eacute;zier&nbsp;Curves
+        </h1>
 
-          <h2 className="font-primary font-normal text-2xl leading-relaxed tracking-[0.25px] mt-[4.0rem]">
-            Circle Basics
-          </h2>
-          <CircleBasicsGridContainer/>
+        <h2 className="font-primary font-normal text-2xl leading-relaxed tracking-[0.25px] mt-[4.0rem]">
+          Circle Basics
+        </h2>
+        <CircleBasicsGridContainer />
 
-          <h2 className="font-primary font-normal text-2xl leading-relaxed tracking-[0.25px] mt-[4.0rem]">
-            Cubic B&eacute;zier
-          </h2>
-          <BezierArcGridContainer />
+        <h2 className="font-primary font-normal text-2xl leading-relaxed tracking-[0.25px] mt-[4.0rem]">
+          Cubic B&eacute;zier
+        </h2>
+        <BezierArcGridContainer />
 
-          <h2 className="font-primary font-normal text-2xl leading-relaxed tracking-[0.25px] mt-[4.0rem]">
-            Rotation
-          </h2>
-          <RotationGridContainer />
-        </MyContainer>
-      </PageWrapper>
+        <h2 className="font-primary font-normal text-2xl leading-relaxed tracking-[0.25px] mt-[4.0rem]">
+          Rotation
+        </h2>
+        <RotationGridContainer />
+      </MyContainer>
     </MathJaxProviderWrapper>
   );
 }
