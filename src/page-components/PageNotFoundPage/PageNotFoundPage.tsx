14b7e4203173a05f311346b2aec4413ec42ee529
Simon Yates <simon@sybaris.ca>
2025-12-28 11:33:51 -0500

    Tweak 404 copy
    Italicize the 'If anyone asks…' line on the 404 page.

--- src/page-components/PageNotFoundPage/PageNotFoundPage.tsx
+++ src/page-components/PageNotFoundPage/PageNotFoundPage.tsx
@@ -5,10 +5,13 @@ export default function PageNotFoundPage() {
   return (
     <MyContainer>
       <h1 className="font-normal leading-none mt-[272px] text-7xl">404</h1>
-      <p className="font-normal leading-normal mb-[388px] mt-[3.1748rem] text-gray-600 text-xl">
-        Sorry, that page doesn&rsquo;t&nbsp;exist.
+      <p className="font-normal leading-relaxed mb-[388px] mt-[3.1748rem] text-gray-600 text-xl">
+        This page does&nbsp;not exist.
         <br />
-        But you can navigate back to&nbsp;
+        <em>If anyone asks, you were never&nbsp;here.</em>
+        <br />
+        <br />
+        You can navigate back&nbsp;to{" "}
         <Link
           href="/"
           className="font-bold text-gray-900 pb-1 hover:text-blue-500 hover:underline"
