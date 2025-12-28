4fb6ccd3eb45a949da0696a60eba93051c19d8d5
Simon Yates <simon@sybaris.ca>
2025-12-28 11:55:43 -0500

    Remove PageWrapper from Home page
    PageWrapper is deprecated/no-op; remove wrapper from HomePage component.

--- src/page-components/HomePage/HomePage.tsx
+++ src/page-components/HomePage/HomePage.tsx
@@ -1,14 +1,11 @@
-import PageWrapper from "../../components/PageWrapper";
 import MyContainer from "../../styling/MyContainer/MyContainer";
 import Content from "./Content";
 
 export function Home() {
   return (
-    <PageWrapper>
-      <MyContainer fullHeight>
-        <Content />
-      </MyContainer>
-    </PageWrapper>
+    <MyContainer fullHeight>
+      <Content />
+    </MyContainer>
   );
 }
 
